f779f8f61e255901805ffcd0d085d3b10f4c6c14
John Wang <johncwang@gmail.com>
2024-01-22 08:09:56 -0800

    feat: `cmd/client`: add example to retrieve OAuth 2.0 bearer token

---
+++ cmd/client/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/saviynt/gosaviynt"
+)
+
+func main() {
+	var paramBaseURL = flag.String("baseurl", "https://example.saviyntcloud.com", "base URL")
+	var paramUsername = flag.String("username", "admin", "usernaem")
+	var paramPassword = flag.String("password", "changeme", "password")
+
+	flag.Parse()
+
+	tok, err := gosaviynt.FetchToken(*paramBaseURL, *paramUsername, *paramPassword)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Retrieved Access Token: %s\n", tok.AccessToken)
+
+	fmt.Println("DONE")
+}
+
+func pointer[E any](e E) *E { return &e }
